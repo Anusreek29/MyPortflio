e284d9c3d4208948441fe9e9e593384ac0aedd93
Anusreek29 <102649723+Anusreek29@users.noreply.github.com>
2022-07-08 11:40:34 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SampleComponent } from './sample/sample.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{path:'About',component:AboutComponent},
+{path:"Sample",component:SampleComponent},
+{path:'Contact',component:ContactComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { SampleComponent } from './sample/sample.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandComponent } from './land/land.component';
+import { ServiceComponent } from './service/service.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TestimonyComponent } from './testimony/testimony.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SampleComponent,
+    ContactComponent,
+    LandComponent,
+    ServiceComponent,
+    HeaderComponent,
+    FooterComponent,
+    TestimonyComponent
   ],
   imports: [
     BrowserModule,
